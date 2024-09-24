3d97506cca27e13d1a2d1ea7a5d3706755b88914
INM-Programming <ineedmoneyagsp@gmail.com>
2024-09-24 12:25:00 -0500

    Link Class

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,15 +1,20 @@
 import my_styles from './mystyles.module.css';
 import Link from 'next/link';
+import Link_Class from '@/class/link_class';
 
 export default function Navbar() {
 
-  const options = ['clock','lottery']
+  var options = [];
+  options.push(new Link_Class('Home','/'));
+  options.push(new Link_Class('Clock','clock'));
+  options.push(new Link_Class('Lottery','lottery'));
+
   return (
     <div className={my_styles.navbar}>
       { options.map(
         (option)=>
-        <Link href={option} className={my_styles.option} key = {option}> 
-          {option}
+        <Link href={option.direction} className={my_styles.option} key = {option.option}> 
+          {option.option}
         </Link>
         )
       }
---
+++ class/link_class.tsx
@@ -0,0 +1,11 @@
+export default class Link_Class {
+    
+    option:string;
+    direction:string;
+
+    constructor(option:string,direction:string) {
+        this.option = option;
+        this.direction = direction;
+    }
+    
+}
