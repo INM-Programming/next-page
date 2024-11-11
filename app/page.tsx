9204f93c87d86ec6b0f1e6d5da0cbeb5d07bb639
INM-Programming <ineedmoneyagsp@gmail.com>
2024-11-10 20:00:33 -0500

    Image incomplete

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import github_logo from "./public/images/github_logo.png"
 export default function Home() {
 
     return (
       <div>
-        Home
+        <Image 
+        src={github_logo} 
+        width={1000} 
+        height={700}
+        alt="Github"/>
       </div>
     );
   }
