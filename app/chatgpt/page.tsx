3f2f76eff57d317050531602f2dedc1db0fccfbc
INM-Programming <ineedmoneyagsp@gmail.com>
2024-11-28 21:58:44 -0500

    ChatBot Incomplete 2

--- app/chatgpt/page.tsx
+++ app/chatgpt/page.tsx
@@ -1,10 +1,16 @@
+'use client'
 import Image from "next/image";
 import chatbot_icon from "../public/images/chatbot_icon.png"
 import person_icon from "../public/images/person_icon.png"
 import chatbot_styles from "./chatgpt.module.css"
+import Message_Class from "../public/classes/message_class";
+import { useState } from "react";
 
 export default function Page(){
 
+    const first_message = new Message_Class("How can I help you?","ChatBot");
+    const [chat,setMessage] = useState <Array<Message_Class>>([first_message]);
+    const [new_message,setNewMessage] = useState <Message_Class>();
     const emissor_icon_style : React.CSSProperties= {
         display: "flex",
         flexGrow: 0,
@@ -14,24 +20,41 @@ export default function Page(){
         width:"20%"
     }
 
+    function sendMessage(message:Message_Class){
+        if(message)
+            setMessage([...chat,message]);
+    }
+
+    function inputMessage(content:string){
+        setNewMessage(new Message_Class(content,"You"));
+    }
+
     return <div className = {chatbot_styles.page}>
-                <div className = {chatbot_styles.bot}>
-                    <div className = {chatbot_styles.emissor_data}>
-                        <Image src = {chatbot_icon} style={emissor_icon_style} alt="Chatbot Icon"/>
-                        <p className = {chatbot_styles.emissor_name}>ChatBot</p>
-                    </div>
-                    <p className = {chatbot_styles.bot_message}> 
-                      How can I help you?
-                    </p>
-                </div>
-                <div className = {chatbot_styles.you}>
-                    <div className = {chatbot_styles.emissor_data}>
-                        <Image src = {person_icon} style={emissor_icon_style} alt="Chatbot Icon"/>
-                        <p className = {chatbot_styles.emissor_name}>You</p>
-                    </div>
-                    <p className = {chatbot_styles.you_message}> 
-                      How can I help you?
-                    </p>
-                </div>
+                {chat.map(
+                    (message,index)=>{
+                        if(message.name_emissor == "ChatBot") 
+                            return <div className = {chatbot_styles.bot} key = {index}>
+                                        <div className = {chatbot_styles.emissor_data}>
+                                            <Image src = {chatbot_icon} style={emissor_icon_style} alt="Chatbot Icon"/>
+                                            <p className = {chatbot_styles.emissor_name}>ChatBot</p>
+                                        </div>
+                                        <p className = {chatbot_styles.bot_message}> 
+                                        {message.content}
+                                        </p>
+                                    </div>; 
+                        else 
+                            return  <div className = {chatbot_styles.you} key = {index}>
+                                        <div className = {chatbot_styles.emissor_data}>
+                                            <Image src = {person_icon} style={emissor_icon_style} alt="Chatbot Icon"/>
+                                            <p className = {chatbot_styles.emissor_name}>You</p>
+                                        </div>
+                                        <p className = {chatbot_styles.you_message}> 
+                                        {message.content}
+                                        </p>
+                                    </div>
+                    }
+                )}
+                <input onChange = {(e)=>inputMessage(e.target.value)} placeholder = "Message"></input>
+                <button onClick = {()=>sendMessage(new_message!)}>Send Message</button>
             </div>
 }
---
+++ app/public/classes/message_class.tsx
@@ -0,0 +1,11 @@
+export default class Message_Class {
+
+    content: string;
+    name_emissor: string;
+
+    constructor(content:string,name_emissor:string){
+        this.content = content;
+        this.name_emissor = name_emissor;
+    }
+
+}
